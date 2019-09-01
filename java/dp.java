4b4a735c0ad91887bdfc4735cb76f24632ca929c
suvojitd79 <suvojit.das@fyle.in>
2019-09-01 14:07:52 +0530

    java-1

--- java/dp.java
+++ java/dp.java
@@ -1,11 +1,12 @@
+import java.util.*;
+public class dp {
+    
 /**
 
- word break problem
+word break problem
 
 */
 
-public class Solution {
-    
     public int wordBreak(String A, ArrayList<String> B) {
         
         int n = A.length();
@@ -46,5 +47,78 @@ public class Solution {
     }
     
     
+
+/**
+ *  FROG JUMP - 
+
+ */
+
+ //DFS solution
+ public boolean canCross(int[] stones) {    
+    if(stones.length==0) return true;
+    
+    HashMap<Integer,HashSet<Integer>> map = new HashMap<>();
+    
+    for(int i=0;i<stones.length;i++)
+        map.put(stones[i], new HashSet<Integer>());
+    
+    HashSet<Integer> data = map.get(stones[0]);
+    data.add(1);
+    
+    
+    for(int i=0;i<stones.length;i++){
+        
+        int stone = stones[i];
+        
+        for(Integer x:map.get(stones[i])){
+            
+                int reach = stone + x;
+            
+                if(reach == stones[stones.length-1]) return true;
+                
+                HashSet<Integer> r = map.get(reach);
+            
+                if(r != null){
+                    
+                    r.add(x);
+                    if(x-1 > 0) r.add(x-1);
+                    r.add(x+1);
+                }
+            
+        }
+        
+        
+    }
+    
+    return false;
 }
+
+
+
+
+/**
+ * 
+ *  Arithmetic Subsequences	
+
+ * 
+ */
+
+
+
+
+
+
+/**
+ *  Maximum sum rectangle in a 2D matrix 
+ * 
+ * 
+ */
+
+
+
+}
+
+
+
+
 
